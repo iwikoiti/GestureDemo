b05fa84fab31ef3d2fe7d9d62cbb533cb823bbb1
yui <yuineor@gmail.com>
2025-12-04 18:01:49 +0700

    scroll image in box

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
@@ -13,6 +14,7 @@ import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -21,6 +23,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,8 +34,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -60,7 +67,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
         //ClickDemo(modifier)
         //TapPressDemo(modifier)
         //DragDemo(modifier)
-        PointerInputDrag(modifier)
+        //PointerInputDrag(modifier)
+        ScrollModifiers(modifier)
+
     }
 }
 
@@ -152,7 +161,7 @@ fun PointerInputDrag(modifier: Modifier = Modifier) {
         Box(
             Modifier
                 .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
-                .background(Color.Blue)
+                .background(Color.Magenta)
                 .size(100.dp)
                 .pointerInput(Unit) {
                     detectDragGestures { _, distance ->
@@ -184,5 +193,30 @@ fun ScrollableModifier(modifier: Modifier = Modifier) {
             .size(90.dp)
             .offset { IntOffset(0, offset.roundToInt()) }
             .background(Color.Red))
+    }
+}
+
+@Composable
+fun ScrollModifiers(modifier: Modifier = Modifier) {
+
+    val image = ImageBitmap.imageResource(id = R.drawable.pom)
+
+    Box(modifier = modifier
+        .size(150.dp)
+        .verticalScroll(rememberScrollState())
+        .horizontalScroll(rememberScrollState())) {
+        Canvas(
+            modifier = Modifier
+                .size(360.dp, 270.dp)
+        )
+        {
+            drawImage(
+                image = image,
+                topLeft = Offset(
+                    x = 0f,
+                    y = 0f
+                ),
+            )
+        }
     }
 }
