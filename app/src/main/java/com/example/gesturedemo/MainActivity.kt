f10bbeb1881669ef26166f025e5b0297647f0aed
yui <yuineor@gmail.com>
2025-12-04 17:42:46 +0700

    detect drag gestures in all directions

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -54,9 +55,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Column {
-        ClickDemo(modifier)
-        TapPressDemo(modifier)
-        DragDemo(modifier)
+        //ClickDemo(modifier)
+        //TapPressDemo(modifier)
+        //DragDemo(modifier)
+        PointerInputDrag(modifier)
     }
 }
 
@@ -133,6 +135,29 @@ fun DragDemo(modifier: Modifier = Modifier) {
                         xOffset += distance
                     }
                 )
+        )
+    }
+}
+
+@Composable
+fun PointerInputDrag(modifier: Modifier = Modifier) {
+
+    Box(modifier = modifier.fillMaxSize()) {
+
+        var xOffset by remember { mutableStateOf(0f) }
+        var yOffset by remember { mutableStateOf(0f) }
+
+        Box(
+            Modifier
+                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
+                .background(Color.Blue)
+                .size(100.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { _, distance ->
+                        xOffset += distance.x
+                        yOffset += distance.y
+                    }
+                }
         )
     }
 }
